8d28d46e2e45c8a39b5bcda7784952fb928d24f7
Sandy Martos <bebesubi2@gmail.com>
2025-04-19 14:33:46 +0200

    adds pdf sharp dependency

---
+++ SoftwareCentral/frmDeliveryDataPdf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CustomControls;
+
+namespace SoftwareCentral
+{
+    public partial class frmDeliveryDataPdf : Form
+    {
+        public frmDeliveryDataPdf()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDeliveryDataPdf_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
